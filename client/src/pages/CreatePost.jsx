5977eba8886894cfc8904efd34bff9db356e739e
AndresGC89 <andresgonzalezchavarria89@gmail.com>
2023-03-30 19:23:05 -0600

    updated ip

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -23,7 +23,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dall-e-eq3w.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-type': 'application/json'
@@ -57,7 +57,7 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true);
 
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://dall-e-eq3w.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-type': 'application/json'
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://dall-e-eq3w.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-type': 'application/json'
